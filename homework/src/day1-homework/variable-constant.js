d198d6aee978391ee95d8f989c6ff6685893ee43
hana12051 <ditn001203@naver.com>
2025-06-14 19:22:29 +0900

    feat: 변수, 상수, 자료형(문자열/숫자/불리언) 실습 코드 작성 -README.md 파일 1번 과제 기준 설명

---
+++ homework/src/day1-homework/variable-constant.js
@@ -0,0 +1,41 @@
+// 자바스크립트 1주차 과제
+
+// 1. 사용자 이름을 저장할 변수를 선언하고 "자신의 이름"을 할당하세요.
+let userName = '조선현'
+console.log(userName)
+
+// 2. "절대 영도(온도의 최저 한계)인 -273.15°C"를 담을 상수를 선언하세요.
+const ABSOLUTE_ZERO = '-273.15°C'
+console.log(ABSOLUTE_ZERO)
+
+// 3. "로그인 여부"를 확인하는 불리언 타입 변수을 선언하고 적절한 값을 할당하세요.
+const isLoggedIn = true
+console.log('로그인에 성공하셨습니다', isLoggedIn)
+
+// 4. "사용자 나이" 변수를 선언하고 숫자 값을 할당하세요.
+let userAge = 25
+console.log(userAge)
+
+// 5. "상품 가격(예: 39,900원)"을 담는 변수를 작성하세요.
+let productPrice = 39_900
+console.log('상품 가격:', productPrice)
+
+// 6. 웹 페이지의 "배경색"을 담는 상수를 작성하고 적절한 값을 할당하세요.
+const BACKGROUND_COLOR = 'black'
+console.log(BACKGROUND_COLOR)
+
+// 7. 사용자가 작성한 "댓글 수(예: 12)"를 담는 변수를 선언하고 값을 할당하세요.
+let commentCount = 12
+console.log(commentCount)
+
+// 8. "현재 페이지 번호(예: 3)"를 담는 변수를 작성하고 적절한 값을 할당하세요.
+let currentPage = 3
+console.log(currentPage)
+
+// 9. "회원 등급('VIP', '골드', '실버' 중 하나)"을 담는 변수를 작성해보세요.
+let membershipLevel = 'VIP'
+console.log(membershipLevel)
+
+// 10. "버튼이 클릭되었는지 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+const isButtonClicked = true
+console.log('버튼 클릭 성공', isButtonClicked)
