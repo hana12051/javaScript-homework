ea7486a3682a320711bfbe695c4610500f0500ab
hana12051 <ditn001203@naver.com>
2025-06-15 16:48:17 +0900

    feat: 주류 구매 가능 여부 판단 함수 과제 제출

---
+++ homework/src/day1-homework/03- function.js
@@ -0,0 +1,9 @@
+const canSellAlcohol = (registrationCard) => {
+  return registrationCard.age >= 19;
+};
+
+const user1 = { name: "선현", age: 25, gender: "여" };
+const user2 = { name: "민지", age: 18, gender: "여" };
+
+console.log(canSellAlcohol(user1)); // true
+console.log(canSellAlcohol(user2)); // false
